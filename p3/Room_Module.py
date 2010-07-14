d7d767bff20b42964482801f9c4006d6aefa97b9
John Murray <murrayj5@nku.edu>
2010-07-13 22:22:24 -0400

    added factory class for Rooms and modified Room class constructor to itialize to lists for various contens vs previous None's

--- p3/Room_Module.py
+++ p3/Room_Module.py
@@ -7,7 +7,7 @@ import random
 
 class Room:
 
-    def __init__(self, description, creature=None, item=None, weapon=None, gold=0):
+    def __init__(self, description, creature=None, item=list(), armor=list(), weapon=list(), gold=0):
         #code here
         self.description = description
         self.creature = creature
@@ -16,19 +16,26 @@ class Room:
         self.gold = gold
 
 
-#static definition to generate random descriptions
-def get_room_description():
-    descrip = list()
-    descrip.append('room 1')
-    descrip.append('room 2')
-    descrip.append('room 3')
-    descrip.append('room 4')
-    descrip.append('room 5')
-    descrip.append('room 6')
-    descrip.append('room 7')
-    descrip.append('room 8')
-    descrip.append('room 9')
-    descrip.append('room 10')
-    descrip.append('room 11')
-    descrip.append('room 12')
-    return descrip[random.randrange(0, len(descrip))]
+class Room_Factory:
+
+    #static definition to generate random descriptions
+    def get_room_description():
+        descrip = list(self)
+        descrip.append('room 1')
+        descrip.append('room 2')
+        descrip.append('room 3')
+        descrip.append('room 4')
+        descrip.append('room 5')
+        descrip.append('room 6')
+        descrip.append('room 7')
+        descrip.append('room 8')
+        descrip.append('room 9')
+        descrip.append('room 10')
+        descrip.append('room 11')
+        descrip.append('room 12')
+        
+        return descrip[random.randrange(0, len(descrip))]
+
+    def generate(self):
+        description = self.get_room_description()
+
