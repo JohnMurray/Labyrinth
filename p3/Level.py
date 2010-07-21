068a488d55b7ed44e82cab8155b448bd9c58d508
John Murray <murrayj5@nku.edu>
2010-07-20 20:29:12 -0400

    labyrinth generation code finished

--- p3/Level.py
+++ p3/Level.py
@@ -8,6 +8,7 @@
 
 import Room_Module
 from Room_Module import *
+import random
 
 
 #Level
@@ -32,6 +33,72 @@ class Level:
                 room = rf.generate()
                 self.rooms[i].append(room)
         self.current = (0, 0)
+
+    #def: rr
+    #purpose: random room (rr) return
+    def rr(self, r):
+        if( random.randrange(100) > 50):
+            return r
+        else:
+            return None
+
+    def gen_lab(self, dimension):
+        self.rooms = list()
+        for i in range(dimension):
+            self.rooms.append(list())
+            for j in range(dimension):
+                rf = Room_Factory()
+                room = rf.generate()
+                if( i == 0 and j == 0 ):
+                    self.rooms[i].append(room)
+                elif( i == dimension - 1 ):
+                    self.rooms[i].append(room)
+                else:
+                    #if room above
+                    if( i > 0 and self.rooms[i - 1][j] != None ):
+                        #if no room above left
+                        if( j == 0 or self.rooms[i - 1][j - 1] == None ):
+                            #if no room above right
+                            if( j == dimension - 1 or self.rooms[i - 1][j + 1] == None ):
+                                self.rooms[i].append(room)
+                            #there is a room above-right
+                            else:
+                                self.rooms[i].append(self.rr(room))
+                        #there is room above-left
+                        else:
+                            k = i - 1
+                            l = j - 1
+                            solved = False
+                            #while there are rooms still to the left
+                            while( l > -1 and self.rooms[k][l] != None and not solved):
+                                #room has a room below
+                                if( k < dimension - 1 and self.rooms[k + 1][l] != None ):
+                                    self.rooms[i].append(self.rr(room))
+                                    solved = True
+                                #room has no room below
+                                else:
+                                    l -= 1
+                            else:
+                                #we did not find a connecting path/room
+                                if( solved == False ):
+                                    self.rooms[i].append(room)
+                    #there are no rooms above
+                    else:
+                        self.rooms[i].append(self.rr(room))
+                                
+
+
+    def __str__(self):
+        out = ''
+        for i in self.rooms:
+            out2 = ''
+            for j in i:
+                if j == None:
+                    out2 += ' '
+                else:
+                    out2 += 'X'
+            out += out2 + "\n"
+        return out
 
     def move_north(self):
         i = self.current[0]
